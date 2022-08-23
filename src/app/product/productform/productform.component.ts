103b14c8c328915e233d122a81a3918f138e4a40
namitrathod1 <namit.rathod@cognizant.com>
2022-08-23 16:34:53 +0530

    productformcomponent1

--- src/app/product/productform/productform.component.ts
+++ src/app/product/productform/productform.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { SharedModule } from 'src/app/shared/shared.module';
 
 
 import { FormBuilder, Validators } from '@angular/forms';
